2103da13c72721c987853263f4fdb7d019af8ff9
cspeisman <cspeisman@gmail.com>
2021-12-25 14:29:26 -0500

    .

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export class NotionFF {
     public db: Set<string> = new Set();
     private readonly user: string;
     private readonly poller: Poller;
+    private callback: any;
 
     constructor(userEmail: string, notionClient?: NotionClientContract, poller?: Poller) {
         this.notion = notionClient
@@ -17,7 +18,7 @@ export class NotionFF {
         this.poller = poller ?? new Poller(notionClient);
     }
 
-    static async initialize(userEmail: string = '', {dbId, authToken, poller}: {dbId: string, authToken?: string, poller?: Poller}) {
+    static async initialize(userEmail: string = '', {dbId, authToken, poller}: {dbId: string, authToken?: string, poller?: Poller}, callback: any) {
         if (!dbId) {
             throw new Error('No DB id was provided, please pass a db id to the constructor');
         }
@@ -26,8 +27,8 @@ export class NotionFF {
         const instance = new NotionFF(userEmail, notionClient, poller);
 
         const rows = await instance.notion.getDatabase(dbId)
+        instance.callback = callback;
         await instance.loadFeatures(rows);
-
         instance.poller.poll(instance.loadFeatures, dbId).catch(console.error);
 
         return instance;
@@ -41,6 +42,7 @@ export class NotionFF {
                 this.db.delete(row.getFeatureName());
             }
         }
+        this.callback(this);
     }
 
     enabled(feature: string): boolean {
